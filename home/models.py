7415de81ace2f8bdc5f37f3e543acb1300df3b13
Mandjo Béa Boré <boremandjo@gmail.com>
2022-01-19 00:09:35 +0100

    Adding a banner and banner fields

--- home/models.py
+++ home/models.py
@@ -10,6 +10,8 @@ from wagtail.admin.edit_handlers import (
     ObjectList,
     TabbedInterface,
 )
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import RichTextField, StreamField
 
 
 class HomePage(Page):
@@ -17,9 +19,26 @@ class HomePage(Page):
     templates = "templates/home/home_page.html"
     max_count = 1
     banner_title = models.CharField(max_length=100, blank=False, null=True)
-
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta"),
 
     ]
 
