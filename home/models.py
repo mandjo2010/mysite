0746272cbfe9f56b527916c5c78ed570d4280387
Mandjo Béa Boré <boremandjo@gmail.com>
2022-01-19 15:23:35 +0100

    Using Orderables in Wagtail CMS

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import (
     FieldPanel,
@@ -13,8 +15,23 @@ from wagtail.admin.edit_handlers import (
 )
 from wagtail.images.edit_handlers import ImageChooserPanel
 
-
 from streams import blocks
+
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    panels = [
+        ImageChooserPanel("carousel_image")
+    ]
 
 
 class HomePage(Page):
@@ -47,10 +64,15 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
+        MultiFieldPanel([
+            FieldPanel("banner_title"),
+            FieldPanel("banner_subtitle"),
+            ImageChooserPanel("banner_image"),
+            PageChooserPanel("banner_cta"),
+        ], heading="Banner Options"),
+        MultiFieldPanel([
+            InlinePanel("carousel_images", max_num=5, min_num=1),
+        ], heading="Carousel Images"),
         StreamFieldPanel("content"),
 
     ]
